f5bc400708aeb3b6f1a8741dcc7dc74490e11a10
lng205 <youbingyoubing@gmail.com>
2022-10-18 15:29:37 +0800

    hw01 completed

--- projects/hw01/hw01.py
+++ projects/hw01/hw01.py
@@ -95,3 +95,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    s = 1
+    print(n)
+    while n > 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        s += 1
+        print(n)
+    return s
